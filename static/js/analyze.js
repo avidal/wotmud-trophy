b1c791dad0f3c3317c49a76dd81d1441f03e6078
Alex Vidal <alex.vidal@gmail.com>
2011-09-05 13:41:32 -0500

    Add output summary.

--- static/js/analyze.js
+++ static/js/analyze.js
@@ -76,3 +76,66 @@ function render_output(entries) {
         $out.trigger('sorton', [[[0,1], [2,1]]]);
     }, 500);
 }
+
+function render_summary(entries) {
+    var $out = $("#player_summary");
+
+    // empty the list
+    $out.children().remove();
+
+    // now build it.
+
+    /*
+     * We are interested in:
+     *  total kills
+     *  unknown kills
+     *  known kills
+     *  number killed once
+     *  number killed > 1
+     *  kill with highest count
+     */
+
+    // First, the total number of kills
+    var total_kills = 0;
+    var unknown_kills = 0;
+    var known_kills = 0;
+
+    var killed_once = 0;
+    var killed_multiple = 0;
+    var killed_most = "";
+    var killed_max = 0;
+
+    $.each(entries, function(name, kills) {
+        if(name.indexOf("#") !== 0) return;
+        total_kills += kills;
+
+        if(kills == 1) {
+            killed_once++;
+        } else {
+            killed_multiple++;
+        }
+
+        if(killed_max < kills) {
+            killed_max = kills;
+            killed_most = name;
+        }
+    });
+
+    if(killed_max) {
+        killed_most = killed_most.substring(1);
+    }
+
+    unknown_kills = entries['#Unknown'] || 0;
+    known_kills = total_kills - unknown_kills;
+
+    var data = [];
+    data.push("You killed a total of " + total_kills + " players.");
+    data.push(unknown_kills + " were unknown.");
+    data.push(known_kills + " were known.");
+    data.push(killed_once + " were killed once.");
+    data.push(killed_multiple + " were killed multiple times.");
+    data.push("<strong>You killed " + killed_most + " the most (" + killed_max + " times!)</strong>");
+
+    $out.append("<li>" + data.join("</li><li>") + "</li>");
+
+}
